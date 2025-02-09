6aaa1a3897eec1b51ea77e6ffe2e21f6f6072c08
Emma Davenport <emma.v.davenport@gmail.com>
2025-02-09 21:03:43 +0000

    and so it begins...
    big dog commit for basic keyscan task

---
+++ software/inc/setup.h
@@ -0,0 +1,8 @@
+#ifndef _SETUP_H
+#define _SETUP_H
+
+#include "midi.h"
+
+void setup();
+
+#endif // _SETUP_H
---
+++ software/inc/tasks.h
@@ -0,0 +1,10 @@
+#ifndef _TASKS_H
+#define _TASKS_H
+
+void sq_play_task();
+
+void key_scan_task();
+
+void menu_task();
+
+#endif // _TASKS_H
