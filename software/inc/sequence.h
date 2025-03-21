9f41cb4382cfd4387b4120882fc39d3ef894ea05
Emma Davenport <emma.v.davenport@gmail.com>
2025-03-21 17:58:29 +0000

    sequence: def STEP_MAX_BYTES, ALL_STEPS_MAX_BYTES
    STEP_MAX_BYTES is the max number of bytes one step
    data can be (see MEMORY.md)
    
    ALL_STEPS_MAX_BYTES is simply the maximum bytes
    per step times the maximum sequence length (max
    steps per sequence)

--- software/inc/sequence.h
+++ software/inc/sequence.h
@@ -3,6 +3,17 @@
 
 #include "FreeRTOS.h"
 #include "semphr.h"
+#include "autoconf.h"
+
+/*
+    STEP_MAX_BYTES is the max number of bytes one step data can be (see
+    MEMORY.md)
+
+    ALL_STEPS_MAX_BYTES is simply the maximum bytes per step times the maximum
+    sequence length (max steps per sequence)
+*/
+#define STEP_MAX_BYTES (3+(3*CONFIG_MAX_POLYPHONY))
+#define ALL_STEPS_MAX_BYTES (STEP_MAX_BYTES * CONFIG_STEPS_PER_SEQUENCE)
 
 void toggle_sequence(uint8_t seq);
 void play_sequences();
