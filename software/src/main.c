fca494564ca76d2e7548aa0c4d645036d7abf9a0
Emma Davenport <emma.v.davenport@gmail.com>
2025-03-28 16:26:57 +0000

    main: remove references to menu task
    the menu handling will be done from the key scan task as that is where
    user inputs are processed. dealing with messages being passed between
    tasks is going to take too much time and slow project momentum

--- software/src/main.c
+++ software/src/main.c
@@ -7,8 +7,6 @@
 #include "setup.h"
 #include "autoconf.h"
 
-TaskHandle_t xTaskHandle_menu = NULL;
-
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
     __disable_irq();
 }
@@ -20,7 +18,6 @@ int main(void) {
 
     // xTaskCreate(sq_play_task, "sq_play_task", 2048, NULL, configMAX_PRIORITIES-1, NULL);
     xTaskCreate(key_scan_task, "key_scan_task", 2048, NULL, 2, NULL);
-    xTaskCreate(menu_task, "menu_task", 2048, NULL, 1, &xTaskHandle_menu);
     vTaskStartScheduler();
     while(1){
 
