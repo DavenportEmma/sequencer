ad074f3fb68201f044457c5eeb560a24c3f9221a
Emma Davenport <emma.v.davenport@gmail.com>
2025-03-28 16:27:00 +0000

    sequence: read steps from memory and play seqs

--- software/src/sequence.c
+++ software/src/sequence.c
@@ -7,7 +7,7 @@
 #include "w25q128jv.h"
 
 extern SemaphoreHandle_t sq_mutex;
-extern volatile MIDISequence_t sq_states[64];
+extern MIDISequence_t sq_states[CONFIG_TOTAL_SEQUENCES];
 
 void toggle_sequence(uint8_t seq) {
     if(xSemaphoreTake(sq_mutex, portMAX_DELAY) == pdTRUE) {
@@ -16,48 +16,79 @@ void toggle_sequence(uint8_t seq) {
     }
 }
 
-static void load_buffer(MIDISequence_t* seq, MIDIPacket_t* buf, uint8_t* len) {
-    uint32_t seq_base_addr = seq->id * CONFIG_SEQ_ADDR_OFFSET;
-    uint32_t step_addr = seq_base_addr + (0x100 * seq->counter);
+static void parse_step_buffer(MIDIChannel_t c, uint8_t* step_buffer, int8_t len) {
+    int byte_index = 0;
 
-    if(++seq->counter > 64) {
-        seq->counter = 0;
+    // hopefully we never run into the case where NOTE_OFF isn't the first byte
+    while(step_buffer[byte_index] != NOTE_OFF) { byte_index++; }
+
+    byte_index++;
+
+    // while after NOTE_OFF and before NOTE_ON send all the note off packet
+    while(step_buffer[byte_index] != NOTE_ON) {
+        MIDIPacket_t p = {
+            .channel = c,
+            .status = NOTE_OFF,
+            .note = step_buffer[byte_index],
+            .velocity = 0,
+        };
+
+        send_midi_note(USART1, &p);
+        byte_index++;
     }
-    // MIDIPacket_t p;
 
-    // p.status = NOTE_OFF;
-    // p.channel = seq->channel;
-    // p.note = A4;
-    // p.velocity = 64;
-    
-    // buf[*len] = p;
-    // (*len)++;
+    byte_index++;
 
-    // p.status = NOTE_ON;
-    // p.channel = CHANNEL_11;
-    // p.note = A4;
-    // p.velocity = 64;
-    
-    // buf[*len] = p;
-    // (*len)++;
+    while(byte_index < len) {
+        MIDIPacket_t p = {
+            .channel = c,
+            .status = NOTE_ON,
+            .note = step_buffer[byte_index],
+            .velocity = step_buffer[byte_index+1],
+        };
+
+        send_midi_note(USART1, &p);
+        // every note on packet takes up two bytes in memory (note and velocity)
+        byte_index+=2;
+    }
+}
+
+static void play_step(uint8_t sq_index) {
+    MIDISequence_t* sq = &sq_states[sq_index];
+
+    // get the address for the step pointer in flash memory
+    // read MEMORY.md for more info
+    uint32_t sq_base_addr = CONFIG_SEQ_ADDR_OFFSET * sq_index;
+    uint32_t steps_base_addr = sq_base_addr + 0x1000;
+    uint32_t current_step_addr = steps_base_addr + sq->counter;
+
+    // see MEMORY.md
+    int8_t buffer_max = 3+(3*CONFIG_MAX_POLYPHONY);
+    uint8_t* step_buffer = (uint8_t*)pvPortCalloc(buffer_max, sizeof(uint8_t));
+
+    // traverse memory and load the step info until and end of step or end of
+    // sequence byte is hit
+    int8_t num_bytes = readStep(current_step_addr, step_buffer, buffer_max);
+
+    // if the end of sequence byte is hit then put the counter back to the start
+    if(step_buffer[num_bytes] == 0xFF) {
+        sq->counter = 0;
+    } else {
+        sq->counter += num_bytes + 1;
+    }
+
+    parse_step_buffer(sq->channel, step_buffer, num_bytes);
+
+    vPortFree(step_buffer);
 }
 
 void play_sequences() {
-    MIDIPacket_t packet_buffer[CONFIG_PACKET_BUFFER_LENGTH];
-    uint8_t buffer_length = 0;
-
     if(xSemaphoreTake(sq_mutex, portMAX_DELAY) == pdTRUE) {
-        for(int i = 0; i < 64; i ++) {
-            if( sq_states[i].enabled &&
-                buffer_length < CONFIG_PACKET_BUFFER_LENGTH
-            ) {
-                load_buffer(&sq_states[i], packet_buffer, &buffer_length);
+        for(int i = 0; i < CONFIG_TOTAL_SEQUENCES; i++) {
+            if(sq_states[i].enabled) {
+                play_step(i);
             }
         }
         xSemaphoreGive(sq_mutex);
-    }
-
-    for(int i = 0; i < buffer_length; i++) {
-        send_midi_note(USART1, &packet_buffer[i]);
     }
 }
