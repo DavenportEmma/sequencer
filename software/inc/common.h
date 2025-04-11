3841db3e368d590d40750e31de92fc197a168cbf
Emma Davenport <emma.v.davenport@gmail.com>
2025-04-11 15:49:33 +0100

    common: add todo

--- software/inc/common.h
+++ software/inc/common.h
@@ -7,6 +7,7 @@
     BYTES_PER_SEQ is the number of bytes per step times the maximum sequence
     length (max steps per sequence)
 */
+// TODO this is bound to change if we add flags for muted/enabled
 #define BYTES_PER_STEP (3+(3*CONFIG_MAX_POLYPHONY))
 #define BYTES_PER_SEQ (BYTES_PER_STEP * CONFIG_STEPS_PER_SEQUENCE)
 
