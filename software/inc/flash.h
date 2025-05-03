a1be746a8c0f4b93de606c61fa408c098fa4e5c1
Emma Davenport <emma.v.davenport@gmail.com>
2025-05-04 00:22:41 +0100

    flash: add wrappers for flash chip functions
    the wrappers let me use mutexes with platform code

---
+++ software/inc/flash.h
@@ -0,0 +1,12 @@
+#ifndef _FLASH_H
+#define _FLASH_H
+
+/*
+    these functions are wrappers for those found in w25q128jv.c in the platform.
+    the wrapping allows for the use of mutexes
+*/
+void flash_eraseSector(uint32_t addr);
+void flash_programPage(uint32_t addr, uint8_t* tx, uint8_t* rx, uint16_t len);
+void flash_SPIRead(uint32_t addr, uint8_t* tx, uint8_t* rx, uint16_t len);
+
+#endif
---
+++ software/src/flash.c
@@ -0,0 +1,26 @@
+#include "w25q128jv.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+extern SemaphoreHandle_t flash_mutex;
+
+void flash_eraseSector(uint32_t addr) {
+    if(xSemaphoreTake(flash_mutex, portMAX_DELAY) == pdTRUE) {
+        eraseSector(addr);
+        xSemaphoreGive(flash_mutex);
+    }
+}
+
+void flash_programPage(uint32_t addr, uint8_t* tx, uint8_t* rx, uint16_t len) {
+    if(xSemaphoreTake(flash_mutex, portMAX_DELAY) == pdTRUE) {
+        programPage(addr, tx, rx, len);
+        xSemaphoreGive(flash_mutex);
+    }
+}
+
+void flash_SPIRead(uint32_t addr, uint8_t* tx, uint8_t* rx, uint16_t len) {
+    if(xSemaphoreTake(flash_mutex, portMAX_DELAY) == pdTRUE) {
+        SPIRead(addr, tx, rx, len);
+        xSemaphoreGive(flash_mutex);
+    }
+}
