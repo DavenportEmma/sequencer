ebddce6c4ed5586ad1dc92cedf0bd01fb198ddd7
Emma Davenport <emma.v.davenport@gmail.com>
2025-05-05 22:55:41 +0100

    util: add one_bit_set()
    this function returns 1 if only one bit is set across the entire bit
    field. it returns 0 if none or multiple bits are set

--- software/inc/util.h
+++ software/inc/util.h
@@ -6,5 +6,6 @@ uint8_t check_bit(uint32_t* field, uint8_t bit, uint8_t max);
 void clear_field(uint32_t* field, uint8_t max);
 void set_bit(uint32_t* field, uint8_t bit, uint8_t max);
 void set_bit_range(uint32_t* field, uint8_t start, uint8_t end, uint8_t max);
+uint8_t one_bit_set(uint32_t* field);
 
 #endif // _UTIL_H
