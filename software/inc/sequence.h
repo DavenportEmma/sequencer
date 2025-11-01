77c471fa370bc894afa4aa8ad376553dc210b508
Emma Davenport <emma.v.davenport@gmail.com>
2025-11-01 22:03:24 +0000

    sequence: add enable_sequence to header
    I forgot to do this before
    
    Signed-off-by: Emma Davenport <emma.v.davenport@gmail.com>

--- software/inc/sequence.h
+++ software/inc/sequence.h
@@ -18,6 +18,7 @@ uint8_t init_sequences();
 uint32_t get_step_data_offset(uint8_t sq_index);
 void toggle_sequence(uint8_t seq);
 void toggle_sequences(uint32_t* select_mask, uint8_t max);
+void enable_sequence(uint8_t sq_index);
 void disable_sequence(uint8_t sq_index);
 void load_sequences(mbuf_handle_t note_on_mbuf, mbuf_handle_t note_off_mbuf);
 void clear_sequence(uint8_t sq_index);
