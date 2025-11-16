e6c5bf671b6c614c33063a7f1af4d6532d979a9a
Emma Davenport <emma.v.davenport@gmail.com>
2025-11-16 21:11:39 +0000

    display: update to use new 16px characters
    Signed-off-by: Emma Davenport <emma.v.davenport@gmail.com>

--- software/inc/display.h
+++ software/inc/display.h
@@ -4,5 +4,7 @@
 void num_to_str(uint8_t n, char* s, uint8_t len);
 void display_line(char* s, uint8_t line);
 void clear_display();
+void diplay_number(uint8_t n, uint8_t line);
+void clear_line(uint8_t line);
 
 #endif // _DISPLAY_H
--- software/src/display.c
+++ software/src/display.c
@@ -19,7 +19,7 @@ void num_to_str(uint8_t n, char* s, uint8_t len) {
     }
 }
 
-void display_line(char* s, uint8_t line) {
+void _display_line(char* s, uint8_t line) {
     uint16_t line_index = line * WIDTH;
     uint16_t eol = line_index + WIDTH-1;
     uint16_t string_index = 0;
@@ -41,5 +41,60 @@ void display_line(char* s, uint8_t line) {
 
 void clear_display() {
     memset(display_buffer, 0, DISPLAY_BUFFER_SIZE);
+}
+
+static uint16_t utf_to_font16(uint16_t utf) {
+    if(utf > 47 && utf < 58) {
+        return (1 + (utf - 48)) * 20;
+    } else if(utf > 64 && utf < 90) {
+        return (11 + (utf - 65)) * 20;
+    } else if(utf == 32) {
+        return 0;
+    } 
+    else {
+        return 0xFFFF;
+    }
+}
+
+void display_line(char* s, uint8_t line) {
+    uint16_t string_index = 0;
+    char c = s[string_index];
+    
+    uint16_t line_index = line * (WIDTH * 2);
+    uint16_t eol = line_index + WIDTH-1;
+    
+    while(c != '\0' && line_index < eol) {
+        uint16_t font16_index = utf_to_font16((uint16_t)(c));
+    
+        memcpy(&display_buffer[line_index], &font16[font16_index], 10);
+        memcpy(&display_buffer[line_index + WIDTH], &font16[font16_index+10], 10);
+
+        c = s[++string_index];
+        line_index += 11;
+    }
+
     display_ssd1306(display_buffer);
+}
+
+void display_number(uint8_t n, uint8_t line) {
+    char s[4];
+    memset(s, '\0', 4);
+    num_to_str(n, s, 3);
+    display_line(s, line);
+}
+
+void clear_line(uint8_t line) {
+    uint16_t line_index = line * (WIDTH * 2);
+
+    memset(&display_buffer[line_index], 0, WIDTH);
+    memset(&display_buffer[line_index+WIDTH], 0, WIDTH);
+}
+
+static void update_display() {
+    display_ssd1306(display_buffer);
+}
+
+void clear_and_update() {
+    clear_display();
+    update_display();
 }
